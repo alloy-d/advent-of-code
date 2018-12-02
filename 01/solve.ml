eec752027309d83b048aa72ee8d9ec6923c3fec9
Adam Lloyd <adam@alloy-d.net>
2018-12-01 22:21:37 -0500

    Refactor, somehow making it bigger.

--- 01/solve.ml
+++ 01/solve.ml
@@ -1,16 +1,25 @@
 open Base
 open Stdio
 
-let determine_final_shift shifts =
-  List.fold shifts
-    ~init:0
-    ~f:(fun freq shift -> freq + shift)
-
 let parsed_shifts shift_strings =
   List.map shift_strings Int.of_string
+
+let frequencies shifts =
+  List.fold shifts
+    ~init:[0]
+    ~f:(fun (previous :: history) shift ->
+      (previous + shift) :: previous :: history
+    )
+
+let format_result result =
+  match result with
+  | None -> "No result!"
+  | Some num -> Int.to_string num
 
 let () =
   In_channel.input_lines stdin
   |> parsed_shifts
-  |> determine_final_shift
-  |> Out_channel.printf "%d"
+  |> frequencies
+  |> List.hd
+  |> format_result
+  |> Out_channel.printf "%s"
