008c3c8d77b048724eb46cbc7d3c55cf7bb032f5
Adam Lloyd <adam@alloy-d.net>
2018-12-01 23:50:38 -0500

    The ugly aftermath of solving 1b.

--- 01/solve.ml
+++ 01/solve.ml
@@ -1,4 +1,4 @@
-open Base
+open Core
 open Stdio
 
 let parsed_shifts shift_strings =
@@ -16,10 +16,33 @@ let format_result result =
   | None -> "No result!"
   | Some num -> Int.to_string num
 
+let shift_streamer pattern =
+  fun index ->
+    List.nth pattern (index mod List.length pattern)
+
+let rec _find_duplicate seen current shifts =
+  let shift = Stream.next shifts in
+  let next = current + shift in
+  if Set.mem seen next
+  then next
+  else _find_duplicate (Set.add seen next) next shifts
+
+let find_duplicate stream =
+  _find_duplicate (Int.Set.of_list [0]) 0 stream
+
+let () =
+  let shifts = parsed_shifts (In_channel.input_lines stdin) in
+  let streamer = shift_streamer shifts in
+  let stream = Stream.from streamer in
+  Out_channel.printf "%d" (find_duplicate stream)
+
+(*
 let () =
   In_channel.input_lines stdin
   |> parsed_shifts
   |> frequencies
-  |> List.hd
+  |> List.rev
+  |> first_duplicate
   |> format_result
   |> Out_channel.printf "%s"
+*)
