6c3e182225fb044d2e26a20505eaf5d98c8d1bb9
Adam Lloyd <adam@alloy-d.net>
2018-12-02 19:43:44 -0500

    Use dune for day 1, too.

--- 01/ocaml/solve.ml
+++ 01/ocaml/solve.ml
@@ -2,13 +2,15 @@ open Core
 open Stdio
 
 let parsed_shifts shift_strings =
-  List.map shift_strings Int.of_string
+  List.map shift_strings ~f:Int.of_string
 
 let frequencies shifts =
   List.fold shifts
     ~init:[0]
-    ~f:(fun (previous :: history) shift ->
-      (previous + shift) :: previous :: history
+    ~f:(fun freqs shift ->
+      match freqs with
+      | (previous :: history) -> (previous + shift) :: previous :: history
+      | [] -> []
     )
 
 let format_result result =
