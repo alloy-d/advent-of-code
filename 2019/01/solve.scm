a11eb1a8e1b5561d9b8d924943c018a610559089
Adam Lloyd <adam@alloy-d.net>
2019-12-02 19:51:37 -0500

    Solve 01b.

--- 2019/01/solve.scm
+++ 2019/01/solve.scm
@@ -3,6 +3,13 @@
 (define (fuel-requirement mass)
   (floor (- (/ mass 3) 2)))
 
+(define (fuel-requirement-including-fuel mass)
+  (let ((fuel (fuel-requirement mass)))
+    (if (>= 0 fuel)
+        0
+        (+ fuel (fuel-requirement-including-fuel fuel)))))
+
 (define (main args)
-  (let ((nums (map string->number (read-lines))))
-    (display (foldl + 0 (map fuel-requirement nums)))))
+  (let ((masses (map string->number (read-lines))))
+    (let ((fuel-for-masses (foldl + 0 (map fuel-requirement-including-fuel masses))))
+      (display fuel-for-masses))))
