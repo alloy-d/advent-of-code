37cd7ab7c8c4488d31d8f946e0233f6ad2953262
Adam Lloyd <adam@alloy-d.net>
2019-12-04 22:04:17 -0500

    03b.

--- 2019/03/solve.scm
+++ 2019/03/solve.scm
@@ -21,13 +21,20 @@
                               (#t              0)))))
     (cons x-movement y-movement)))
 
+(define integer-comparator
+  (make-comparator integer? = < number-hash))
 (define space-comparator
-  (let ((coord-comparator
-          (make-comparator integer? = < number-hash)))
-    (make-pair-comparator coord-comparator coord-comparator)))
+  (make-pair-comparator integer-comparator integer-comparator))
+(define space-with-distance-comparator
+  (make-pair-comparator
+    space-comparator
+    (make-comparator integer?
+                     (lambda (x y) #t) ; we just care that it exists
+                     (lambda (x y) #t)
+                     (lambda (x) 0))))
 
 (define (make-space-set)
-  (set space-comparator))
+  (set space-with-distance-comparator))
 
 (define (add-coords coord1 coord2)
   (cons (+ (car coord1) (car coord2))
@@ -42,18 +49,28 @@
   (cons (next-int-toward-zero (car movement))
         (next-int-toward-zero (cdr movement))))
 
+(define space-with-distance cons)
 (define (spaces-covered-by-movement start movement)
   (let loop ((spaces (make-space-set))
-             (movement movement))
+             (movement movement)
+             (distance-traveled
+               (+ (cdr start)
+                  (manhattan-distance-from-origin movement))))
     (if (equal? movement '(0 . 0))
         spaces
-        (let ((current-space (add-coords start movement)))
+        (let ((current-space (space-with-distance
+                               (add-coords (car start) movement)
+                               distance-traveled)))
           (loop (set-adjoin spaces current-space)
-                (next-smaller-movement movement))))))
+                (next-smaller-movement movement)
+                (- distance-traveled 1))))))
+
+(define (manhattan-distance-from-origin coords)
+  (+ (abs (car coords)) (abs (cdr coords))))
 
 (define (spaces-covered-by-movements movements)
   (let account-for-movements ((movements movements)
-                              (pos '(0 . 0))
+                              (pos (space-with-distance '(0 . 0) 0))
                               (spaces-covered-previously (make-space-set)))
     (if (empty? movements)
         spaces-covered-previously
@@ -61,17 +78,29 @@
               (remaining-movements (cdr movements)))
           (account-for-movements
             remaining-movements
-            (add-coords current-movement pos)
-            (set-union (spaces-covered-by-movement pos current-movement)
-                       spaces-covered-previously))))))
+            (space-with-distance
+              (add-coords current-movement (car pos))
+              (+ (manhattan-distance-from-origin current-movement)
+                 (cdr pos)))
+            (set-union spaces-covered-previously
+                       (spaces-covered-by-movement pos current-movement)))))))
 
 ; Here, a "wire" is a list of movements.
-(define (find-intersections wires)
-  (let ((spaces-covered-per-wire (map spaces-covered-by-movements wires)))
-    (apply set-intersection spaces-covered-per-wire)))
+(define (spaces-covered-per-wire wires)
+  (map spaces-covered-by-movements wires))
 
-(define (manhattan-distance-from-origin coords)
-  (+ (abs (car coords)) (abs (cdr coords))))
+(define (find-intersections spaces-covered-per-wire)
+  (apply set-intersection spaces-covered-per-wire))
+
+(define (distance-to-space space spaces-covered)
+  (cdr (set-member spaces-covered
+                   space
+                   (space-with-distance '(0 . 0) +inf.0))))
+
+(define (combined-distance-to-space space coverages)
+  (apply + (map (lambda (spaces-covered)
+                  (distance-to-space space spaces-covered))
+                coverages)))
 
 (define (parse-input-line line)
   (map parse-movement (string-split line ",")))
@@ -86,7 +115,16 @@
   (with-input-from-file "input" read-input-lines))
 
 (define (main args)
-  (display "manhattan distance to nearest intersection: ")
-  (display
-    (apply min (map manhattan-distance-from-origin
-                    (set->list (find-intersections input))))))
+  (let* ((coverages (spaces-covered-per-wire input))
+         (intersections (find-intersections coverages)))
+    (display "manhattan distance to nearest intersection: ")
+    (display
+      (apply min (map (lambda (space)
+                        (manhattan-distance-from-origin (car space)))
+                      (set->list intersections))))
+    (newline)
+    (display "fewest combined steps to get to an intersection: ")
+    (display
+      (apply min (map (lambda (space)
+                        (combined-distance-to-space space coverages))
+                      (set->list intersections))))))
